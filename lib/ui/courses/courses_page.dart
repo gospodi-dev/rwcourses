d6707a0c2e82276fdf3f732f605a770f89f915a3
Gospodi <gospodijazzet@gmail.com>
2022-06-09 20:21:46 +0300

    добавлен виджет с отслеживанием состояния

---
+++ lib/ui/courses/courses_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CoursesPage extends StatefulWidget {
+  const CoursesPage({Key? key}) : super(key: key);
+
+  @override
+  State<CoursesPage> createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> {
+  @override
+  Widget build(BuildContext context) {}
+}
