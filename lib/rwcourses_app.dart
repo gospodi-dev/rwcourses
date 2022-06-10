d82962a3873e42eeb5e20ef57cb7701c3420b9a2
Gospodi <gospodijazzet@gmail.com>
2022-06-10 22:56:53 +0300

    выводи ответ на страницу с курсами

--- lib/rwcourses_app.dart
+++ lib/rwcourses_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:rwcourses/strings.dart';
+import 'ui/courses/courses_page.dart';
 
 class RWCoursesApp extends StatelessWidget {
   const RWCoursesApp({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class RWCoursesApp extends StatelessWidget {
       appBar: AppBar(
         title: Text(Strings.appTitle),
       ),
+      body: const CoursesPage(),
     );
   }
 }
