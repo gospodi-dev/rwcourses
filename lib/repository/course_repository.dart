954fcbf8ff33d801d888a624c0548ec37205d48c
Gospodi <gospodijazzet@gmail.com>
2022-06-10 22:21:05 +0300

    имплиментируем загрузку курсов

--- lib/repository/course_repository.dart
+++ lib/repository/course_repository.dart
@@ -1,3 +1,4 @@
+import 'package:rwcourses/model/course.dart';
 import 'repository.dart';
 
 // класс хранилища курсов
@@ -5,4 +6,11 @@ import 'repository.dart';
 class CourseRepository implements Repository {
   String dataURL =
       'https://api.raywenderlich.com/api/contents?filter[content_types][]=collection';
+
+  @override
+  Future<List<Course>> getCourses(int domainFilter) async {
+    final courses = <Course>[];
+
+    return courses;
+  }
 }
