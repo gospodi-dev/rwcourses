2468aef2c1599993cdc2cf3d9d48e7ae9925dd01
Gospodi <gospodijazzet@gmail.com>
2022-06-10 22:45:54 +0300

    добавляем метод возвращ. список курсов в будущем

--- lib/ui/courses/courses_controller.dart
+++ lib/ui/courses/courses_controller.dart
@@ -2,4 +2,11 @@ import '../../repository/repository.dart';
 import '../../repository/course_repository.dart';
 import '../../model/course.dart';
 
-class CoursesController {}
+class CoursesController {
+  final Repository _repository = CourseRepository();
+
+  Future<List<Course>> fetchCourses(int domainFilter) {
+    return _repository
+        .getCourses(domainFilter); // метод возвращающий список курсов в будущем
+  }
+}
