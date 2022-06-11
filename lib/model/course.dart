705e3592b8e4fbf23caae3b48897cf9d13ba7331
Gospodi <gospodijazzet@gmail.com>
2022-06-11 20:37:56 +0300

    обновим конструктор

--- lib/model/course.dart
+++ lib/model/course.dart
@@ -12,7 +12,10 @@ class Course {
   Course.fromJson(Map<String, dynamic> json)
       : courseId = json['id'] as String,
         name = json['attributes']['name'] as String,
-        description = json['attributes']['description_plain_text'] as String;
+        description = json['attributes']['description_plain_text'] as String,
+        artworkUrl = json['attributes']['card_artwork_url'] as String,
+        difficulty = json['attributes']['difficulty'] as String,
+        contributors = json['attributes']['contributor_string'] as String;
 
   @override
   String toString() {
