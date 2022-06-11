7ca21a27e592f90aa9ecc318bf5cbb4970c42c09
Gospodi <gospodijazzet@gmail.com>
2022-06-11 19:59:10 +0300

    обновляем вывод информации на страницу

--- lib/repository/course_repository.dart
+++ lib/repository/course_repository.dart
@@ -22,7 +22,10 @@ class CourseRepository implements Repository {
     final uri = Uri.parse(url); // преобразуем полученный url
     final response = await http.get(uri); // делаем фактический запрос
     final apiResponse = json.decode(response.body) as Map<String, dynamic>;
-    print(apiResponse);
+    final data = apiResponse['data'] as List;
+    for (final json in data) {
+      courses.add(Course.fromJson(json as Map<String, dynamic>));
+    }
 
     return courses;
   }
