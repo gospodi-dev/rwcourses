d9fa401004dce74a1cffe5e55f8776ca7df9d8a0
Gospodi <gospodijazzet@gmail.com>
2022-06-13 21:52:24 +0300

    обновили свойство и удалили импорт

--- lib/ui/courses/courses_controller.dart
+++ lib/ui/courses/courses_controller.dart
@@ -1,9 +1,8 @@
 import '../../repository/repository.dart';
-import '../../repository/course_repository.dart';
 import '../../model/course.dart';
 
 class CoursesController {
-  final Repository _repository = CourseRepository();
+  final Repository _repository;
 
   Future<List<Course>> fetchCourses(int domainFilter) {
     return _repository
