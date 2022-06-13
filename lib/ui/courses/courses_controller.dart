f05939370144312d67523d90de42c378c5e43f55
Gospodi <gospodijazzet@gmail.com>
2022-06-13 21:53:04 +0300

    добавлен конструктор для контроллера

--- lib/ui/courses/courses_controller.dart
+++ lib/ui/courses/courses_controller.dart
@@ -4,6 +4,8 @@ import '../../model/course.dart';
 class CoursesController {
   final Repository _repository;
 
+  CoursesController(this._repository);
+
   Future<List<Course>> fetchCourses(int domainFilter) {
     return _repository
         .getCourses(domainFilter); // метод возвращающий список курсов в будущем
