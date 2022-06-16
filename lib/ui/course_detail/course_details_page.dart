37f4a18c2988ea7861e72478bef27a9f1417520d
Gospodi <gospodijazzet@gmail.com>
2022-06-16 21:25:28 +0300

    создали свойство для хранения объекта курса

--- lib/ui/course_detail/course_details_page.dart
+++ lib/ui/course_detail/course_details_page.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:rwcourses/constants.dart';
+import 'package:rwcourses/model/course.dart';
 
 class CourseDetailsPage extends StatelessWidget {
   const CourseDetailsPage({Key? key}) : super(key: key);
+
+  final Course course;
 
   @override
   Widget build(BuildContext context) {
