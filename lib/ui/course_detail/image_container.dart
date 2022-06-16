ac237444768e28125eb37185c8cdfcb2279e8d2c
Gospodi <gospodijazzet@gmail.com>
2022-06-16 21:44:05 +0300

    добавлены свойства для изображения

--- lib/ui/course_detail/image_container.dart
+++ lib/ui/course_detail/image_container.dart
@@ -3,6 +3,11 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ImageContainer extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final Color placeholder;
+  final String url;
+
   const ImageContainer({Key? key}) : super(key: key);
 
   @override
