9a30f3d27b8856500b84d259de6c567e74f9be26
Gospodi <gospodijazzet@gmail.com>
2022-06-16 21:23:41 +0300

    добавлена страница для вывода деталей курса

---
+++ lib/ui/course_detail/course_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:rwcourses/constants.dart';
+
+class CourseDetailsPage extends StatelessWidget {
+  const CourseDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
