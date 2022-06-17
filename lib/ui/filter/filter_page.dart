09d491eb42591a4ee0215c5240a68c8529f0b7d9
Gospodi <gospodijazzet@gmail.com>
2022-06-17 12:10:26 +0300

    добавлен виджет для отображения содерж. фильтра

--- lib/ui/filter/filter_page.dart
+++ lib/ui/filter/filter_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:rwcourses/strings.dart';
 
 class FilterPage extends StatefulWidget {
   const FilterPage({Key? key}) : super(key: key);
@@ -12,6 +13,11 @@ class FilterPage extends StatefulWidget {
 class _FilterPageState extends State<FilterPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.filter),
+      ),
+      body: const Text('Filter'),
+    );
   }
 }
