dbecccd35142c488756d526cc6776cdf594bd34f
Gospodi <gospodijazzet@gmail.com>
2022-06-17 12:17:54 +0300

    добавили значок фильра с переходом на страницу

--- lib/rwcourses_app.dart
+++ lib/rwcourses_app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:rwcourses/strings.dart';
+import 'package:rwcourses/ui/filter/filter_page.dart';
 import 'ui/courses/courses_page.dart';
 
 class RWCoursesApp extends StatelessWidget {
@@ -12,6 +14,14 @@ class RWCoursesApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.appTitle),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.filter_list),
+            onPressed: () => Navigator.of(context).push<MaterialPageRoute>(
+              MaterialPageRoute(builder: (_) => const FilterPage()),
+            ),
+          )
+        ],
       ),
       body: const CoursesPage(),
     );
